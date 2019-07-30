047b717803cce7362e2c26eb553506fc5d5309d7
ZGong1 <43224451+ZGong1@users.noreply.github.com>
2019-07-30 00:41:41 -0500

    added give command and updated how the creepers behave

--- NukeCreeper/src/com/javaminecraft/NukeCreeper.java
+++ NukeCreeper/src/com/javaminecraft/NukeCreeper.java
@@ -7,12 +7,16 @@ import org.bukkit.event.*;
 import org.bukkit.event.entity.*;
 import org.bukkit.plugin.*;
 import org.bukkit.entity.*;
+import org.bukkit.inventory.*;
+import org.bukkit.potion.*;
+import org.bukkit.inventory.meta.*;
+
+//put custom creeper name in the config file
+//pug amount of ticks lived in config file
+
 
 public class NukeCreeper extends JavaPlugin implements Listener {
     
-    Player me;
-    World world;
-    Location spot;
     
     @Override
     public void onEnable() {
@@ -23,10 +27,24 @@ public class NukeCreeper extends JavaPlugin implements Listener {
     
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        
+        
         if(args.length == 1 && label.equalsIgnoreCase("CreeperNuke") && args[0].equals("reload")) {
             this.reloadConfig();
             sender.sendMessage("The config file was successfully reloaded");
             return true;
+        }
+        
+        if(sender instanceof Player) {
+            if(args.length == 1 && label.equalsIgnoreCase("CreeperNuke") && args[0].equals("give")) {
+                Player me = ((Player) sender);
+                ItemMeta im = me.getServer().getItemFactory().getItemMeta(Material.CREEPER_SPAWN_EGG);
+                im.setDisplayName(this.getConfig().getString("name"));
+                ItemStack is = new ItemStack(Material.CREEPER_SPAWN_EGG);
+                is.setItemMeta(im);
+                me.getInventory().addItem(is);
+                return true;
+            }   
         }
         
         return false;
@@ -36,11 +54,13 @@ public class NukeCreeper extends JavaPlugin implements Listener {
     @EventHandler
     public void ExplosionPrimeEvent(ExplosionPrimeEvent event) {
         //checks if exploding thing is a creeper
-        if(event.getEntity().getName().equals("C4EEP3–R")) {
+        if(event.getEntity().getName().equals(this.getConfig().getString("name"))) {
+            Location loc = event.getEntity().getLocation();
             if(event.getEntity().getTicksLived() < 1200) {
                 if(event.getEntity().getCustomName() != null) {
                     event.setRadius(this.getConfig().getInt("radius"));
                     event.setFire(true);
+                    event.getEntity().getServer().broadcastMessage("A nuke was set off at " + loc.getX() + ", " + loc.getY() + ", " + loc.getX() + ".");
                 }
             }
         }
@@ -51,6 +71,22 @@ public class NukeCreeper extends JavaPlugin implements Listener {
         if(event.getDamager().getName().equals("C4EEP3–R")) {
             event.setCancelled(true);
         }
-    } 
+    }
+    
+    @EventHandler
+    public void EntitySpawnEvent(EntitySpawnEvent event) {
+        Entity target = event.getEntity();
+        World world = target.getWorld();
+        Location loc = target.getLocation();
+        if(target.getName().equals(this.getConfig().getString("name"))) {
+            PotionEffect pot = new PotionEffect(PotionEffectType.SLOW, this.getConfig().getInt("secondsToExplode"), 255);
+            world.strikeLightningEffect(loc);
+            Creeper cr = (Creeper)target;
+            cr.setPowered(true);
+            pot.apply(cr);
+            cr.setMaxFuseTicks(this.getConfig().getInt("secondsToExplode") * 20);
+            cr.ignite();
+        }
+    }
     
 }
